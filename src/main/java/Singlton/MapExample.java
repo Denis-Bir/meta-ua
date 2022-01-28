cd36ef79a4f949f1c711e53b3a752ff50c806bb5
Denis-Bir <birukovdenis1980@gmail.com>
2022-01-28 16:11:12 +0200

    collections

--- src/main/java/Singlton/MapExample.java
+++ src/main/java/Singlton/MapExample.java
@@ -1,15 +1,17 @@
 package Singlton;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.*;
 
 
 public class MapExample {
 
     public static void main(String[] args) {
         String text = "tools toys test for_test before_test alphabet enter another ";
+
         calculateText(text);
+
+        List<String> col = new ArrayList<String> (Arrays.asList("one", "two", "three", "two"));
+        System.out.println(hasDuplicates(col));
 
     }
 
@@ -28,9 +30,18 @@ public class MapExample {
         }
     }
 
+    private static boolean hasDuplicates(Collection col) {
+        //Set set = (Set) col.stream().collect(Collectors.toSet());
+        //return set.size() != col.size();
 
-    //private static boolean hasDuplicates(Collection col) {
-    
+        Set set1 = new HashSet();
+        for (Object item: col) {
+            set1.add(item);
+        }
+
+        return set1.size() != col.size();
+    }
+
 
 
 }
